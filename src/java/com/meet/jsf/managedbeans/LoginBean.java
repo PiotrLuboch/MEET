6e2b77a2805456caa5c2bf3dd83af938ea791793
Piotr Luboch <200552@student.pwr.edu.pl>
2016-04-18 10:30:40 +0200

    login and register page done

--- src/java/com/meet/jsf/managedbeans/LoginBean.java
+++ src/java/com/meet/jsf/managedbeans/LoginBean.java
@@ -5,10 +5,72 @@
  */
 package com.meet.jsf.managedbeans;
 
+import com.meet.jsf.navigation.Navigator;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 /**
  *
  * @author Piotr
  */
+@ManagedBean
+@SessionScoped
 public class LoginBean {
-    
+
+    @Pattern(regexp = "^\\w*$", message = "Login can contain only letters and numbers")
+    @Size(min = 4, max = 16, message = "Login length must be in range from 4 to 16")
+    private String username;
+    @Size(min = 4, max = 16, message = "Password length must be in range from 4 to 16")
+    private String password;
+    private boolean isLoggedIn = false;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isIsLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setIsLoggedIn(boolean isLoggedIn) {
+        this.isLoggedIn = isLoggedIn;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String userName) {
+        this.username = userName;
+    }
+
+    public String login() {
+        if (username.equals("admin") && password.equals("admin")) {
+            isLoggedIn = true;
+        } else {
+            FacesContext.getCurrentInstance().addMessage(
+                    null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN,
+                            "Incorrect username or password",
+                            "Please enter valid username and password"));
+            return Navigator.toLogin();
+        }
+        return Navigator.toIndex();
+    }
+
+    public String logout() {
+        isLoggedIn = false;
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+        session.invalidate();
+        return Navigator.toLogin();
+    }
 }
---
+++ src/java/com/meet/jsf/navigation/Navigator.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.meet.jsf.navigation;
+
+/**
+ *
+ * @author Piotr
+ */
+
+public class Navigator {
+    private Navigator(){}
+    
+    public static String toIndex(){
+        return "index";
+    }
+    
+    public static String toLogin(){
+        return "login";
+    }
+    
+    public static String toRegister(){
+        return "register";
+    }
+    
+    public static String toAccount(){
+        return "account";
+    }
+}
